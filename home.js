f567d7a15e58d92c861b3a04796ca467cd6f1f24
daymonjohnson <38265232+daymonjohnson@users.noreply.github.com>
2018-06-20 14:45:27 -0400

    Add files via upload

--- home.js
+++ home.js
@@ -1,56 +1,61 @@
 "use strict";
 {
-    
-    
-    
     let home = {
         template: `<div>
         <h1>Hi the API is working!</h1>
-        <input type="text" placeholder="Enter an artist" ng-model="$ctrl.artist"/><button ng-click="$ctrl.getTrackId(); $ctrl.callLyrics()">Submit</button>
+        <input type="text" placeholder="Enter an artist" ng-model="$ctrl.artist"/><button ng-click="$ctrl.getTrackId($ctrl.songNum)">Submit</button>
         <p>Lyric: {{$ctrl.lyrics}}</p>
-        <p> Song: {{$ctrl.songNum}}</p>
+        <p> ID: {{$ctrl.songNum}}</p>
+        <p> Song Name: {{$ctrl.songName}}</p>
+        <input type = "text" placeholder="Guess the song" ng-model="$ctrl.guess"><button ng-click="$ctrl.getSongName($ctrl.songNum)">GEET'EM</button>
         </div>`,
-
-        controller: function(service) {
-            let vm = this;   
+// empty strings are created here that are filled in when a specific artist, lyric, guess is called. 
+        controller: function (service) {
+            let vm = this;
             vm.artist = "";
-            
-            // vm.callAPI = service.getAPI();
-            vm.callLyrics = function(){
-                service.getLyrics(service.beId())
-                .then(function(response){
-                    
-                    vm.lyrics = response;
+            vm.lyrics = "";
+            vm.guess = "";
+// guessSong function will determine if the users answer is correct or not and give an appropriate response
+            vm.guessSong = function (guess) {
+                if (guess.toLowerCase() == vm.songName.toLowerCase()) {
+                    console.log("correct");
+                } else {
+                    // console.log("guess again");
+                }
+            }
+// getLyrics using the trackId to get lyrics
+            vm.getLyrics = function () {
+                service.getLyrics(vm.songNum).then(function () {
+                    vm.lyrics = service.beLyrics();
                     return vm.lyrics;
                 });
-            } 
-
-            vm.getTrackId = function(artist){
-                service.getTrackId(vm.artist)
-                .then(function(response){
-                    vm.songNum= response; 
-                    service.beId();
-                    service.setId(vm.songNum);
-                });
             }
-            // vm.callAPI.then(function(response){
-            //     vm.song = response;
-            // });
-            // vm.callLyrics().then(function(response){
-            //     console.log("hi");
-            //     vm.lyrics = response;
-            // });
+// getSongName using the trackId to getSongName
+            vm.getSongName = function () {
+                service.getSongName(vm.songNum).then(function (response) {
+                    vm.songName = response;
+                    vm.guessSong(vm.guess);
+                    console.log(vm.guess);
+                    return vm.songName;
+                    
+                });                
+            }
+// getTrackId is giving the song a trackId based on the artist name
+            vm.getTrackId = function () {
+                service.getTrackId(vm.artist)
+                    .then(function (response) {
+                        vm.songNum = response;
+                        vm.getLyrics();
+                        return vm.songNum;
+                    });
+            }
 
-//             vm.callAPI.then(function(response){
-//                 vm.song = response;
-//             });
-// >>>>>>> ecff9b3566a0151e996847424a59d4b96a807cac
-		}
-	};
+        }
+    };
 
     home.$inject = ["service"];
 
     angular
-    .module("app")
-    .component("home", home)
+        .module("app")
+        .component("home", home)
 }
