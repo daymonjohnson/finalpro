145c3c7428c10e655c327884d4094d9b7dfb61d6
daymonjohnson <38265232+daymonjohnson@users.noreply.github.com>
2018-06-22 13:33:21 -0400

    Add files via upload

--- game.js
+++ game.js
@@ -6,12 +6,12 @@
             <div class="tracker">
                 <span ng-repeat="conds in $ctrl.count"class = "winContainer"><i class={{conds.class}}></i></span>
             </div>
-
             <div class="content">
                 <p>Lyric: {{$ctrl.lyrics}} </p>
                 <input class = "input" type = "text" placeholder="Guess the song" ng-model="$ctrl.guess"><button class="mybtn" ng-click="$ctrl.getSongName($ctrl.songNum); $ctrl.getTrackId($ctrl.songNum)">GEET'EM</button>
                 <p> Result: {{$ctrl.result}} {{$ctrl.condition}}</p>
             </div>
+            <div class="modal" ng-show="$ctrl.show">{{$ctrl.modalText}}<img src="{{$ctrl.resultImg}}" width="200px"><a href="#!/home"><button>Play again?</button></a></div>
 
         </div>
         `,
@@ -27,23 +27,30 @@
             vm.losses = 0;
             vm.condition = "";
             vm.count = [];
+            vm.modalText = "";
+            vm.resultImg = "";
             // guessSong function will determine if the users answer is correct or not and give an appropriate response
             vm.guessSong = function (guess) {
                 if (guess.toLowerCase() == vm.songName.toLowerCase()) {
                     console.log("correct");
                     vm.result = "correct";
                     vm.wins++;
-                    if (vm.wins === 3) {
+                    if (vm.wins === 7) {
                         vm.condition = "you win!";
                         vm.wins = 0;
                         vm.losses = 0;
+                        //disables button after 10 entries
+                        $(".mybtn").prop('disabled', true);
+                        vm.modalText = "You win!";
+                        vm.resultImg = "https://i.gifer.com/Wvua.gif";
+                        vm.show =! vm.show;
                     }
-                    vm.count.push({class: "fas fa-trophy"});
+                    vm.count.push({ class: "fas fa-trophy" });
                     console.log(vm.count);
                     return vm.result;
                 } else {
                     console.log("guess again");
-                    vm.count.push({class: "fas fa-times"});
+                    vm.count.push({ class: "fas fa-times" });
                     console.log(vm.count);
                     vm.result = "Do you even listen to " + service.beArtist() + "? The answer was: " + vm.songName + ".";
                     vm.losses++;
@@ -52,12 +59,17 @@
                         // if user lose count reaches 3 reset wins and losses 0. Reset start again. 
                         vm.wins = 0;
                         vm.losses = 0;
+                        $(".mybtn").prop('disabled', true);
+                        vm.modalText = "You are such a loser";
+                        vm.resultImg = "https://media1.popsugar-assets.com/files/thumbor/B8lOru9rf0j1aOafrCxnqK6fKeA/fit-in/1200x630/filters:format_auto-!!-:strip_icc-!!-:fill-!white!-/2017/05/12/985/n/3019466/53c5add94ca9772d_pizza2/i/Pizza-you-when-youre-sad.gif";
+                        vm.show =! vm.show;
+                        vm.background =! vm.background;
 
                     }
                     return vm.result;
                 }
             }
-            
+
             // getLyrics using the trackId to get lyrics
             vm.getLyrics = function () {
                 service.getLyrics(vm.songNum).then(function () {
@@ -95,12 +107,14 @@
                 $("input").val("");
             });
 
+
+
             $('input').keypress(function (e) {
                 if (e.which == 13) {
-                  $('button').click();
-                  return false;
+                    $('button').click();
+                    return false;
                 }
-              });
+            });
 
         }
     };
