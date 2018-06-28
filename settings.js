1f35d4c52c534e574a04aeb5a4fb45927f943f3d
daymonjohnson <38265232+daymonjohnson@users.noreply.github.com>
2018-06-27 22:35:50 -0400

    Add files via upload

--- settings.js
+++ settings.js
@@ -1,14 +1,23 @@
 "use strict";
 {
     let settings = {
-        template: `<div class="settingsContent">
-        <h1>Select your Difficulty</h1>
-        <a href="#!/game"><button class="difficultyButton" ng-mouseenter="val=true" ng-mouseleave="val=false" ng-click="$ctrl.difficulty(3,3)">Easy <span ng-show="val"> {{$ctrl.text}}</span></button><a>        
-        <a href="#!/game"> <button class="difficultyButton" ng-mouseenter="val2=true" ng-mouseleave="val2=false" ng-click="$ctrl.difficulty(5,3)">Medium <span ng-show="val2"> {{$ctrl.text2}}</span></button></a>        
-        <a href="#!/game"> <button class="difficultyButton" ng-mouseenter="val3=true" ng-mouseleave="val3=false" ng-click="$ctrl.difficulty(7,3)">Hard <span ng-show="val3"> {{$ctrl.text3}}</span></button></a>
-        <a href="#!/game"> <button class="difficultyButtonInsane" ng-mouseenter="val4=true" ng-mouseleave="val4=false" ng-click="$ctrl.difficulty(10,1)">Insane <span ng-show="val4"> {{$ctrl.text4}}</span></button></a>
+        template: `<h1>Select your Difficulty</h1>
+        <div class="settingsContainer">
+        <div class="settingsContent">
+            <div>
+                <a href="#!/game"><button class="difficultyButton" ng-mouseenter="val=true" ng-mouseleave="val=false" ng-click="$ctrl.difficulty(3,3)">Easy <span ng-show="val"> {{$ctrl.text}}</span></button><a>        
+            </div>
+            <div>
+                <a href="#!/game"> <button class="difficultyButton" ng-mouseenter="val2=true" ng-mouseleave="val2=false" ng-click="$ctrl.difficulty(5,3)">Medium <span ng-show="val2"> {{$ctrl.text2}}</span></button></a>        
+            </div>
+            <div>
+                <a href="#!/game"> <button class="difficultyButton" ng-mouseenter="val3=true" ng-mouseleave="val3=false" ng-click="$ctrl.difficulty(7,3)">Hard <span ng-show="val3"> {{$ctrl.text3}}</span></button></a>
+            </div>
+            <div>   
+                <a href="#!/game"> <button class="difficultyButton" id="difficultyButtonInsane" ng-mouseenter="val4=true" ng-mouseleave="val4=false" ng-click="$ctrl.difficulty(10,1)">Insane <span ng-show="val4"> {{$ctrl.text4}}</span></button></a>
+            </div>        
         </div>
-        <div class="footer"><em><i class="fas fa-code"></i> with <i class="fas fa-heart"></i> from Claire, Kelly, DJ, and Roger!</em></div>`,
+        <div class="footer"><em><i class="fas fa-code"></i> with <i class="fas fa-heart"></i> from Claire, Kelly, DJ, and Roger!</em></div></div>`,
 
 
         controller: function(service){
